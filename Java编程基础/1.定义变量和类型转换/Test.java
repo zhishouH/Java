1231c85f3d6b328935fbc6fba850437b3674c929
zhishouhuang <zhishouhuang@gmail.com>
2020-05-06 16:46:29 +0800

    定义变量和类型转换

---
+++ Java编程基础/1.定义变量和类型转换/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+//    定义一个初始值为3.5的浮点型变量number1，将该变量强制转换为整型，并赋值给整型变量number2，
+    public static void main(String[] args) {
+        float number1 = 3.5f;
+        int number2;
+        number2 = (int)number1;
+        System.out.println("number2 = " + number2);
+    }
+}
