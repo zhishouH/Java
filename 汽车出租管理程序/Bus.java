825024476cdcdee8ab4208bc17b2cbc23c10adb7
zhishouhuang <zhishouhuang@gmail.com>
2020-06-13 21:54:35 +0800

    汽车父类

---
+++ 汽车出租管理程序/Bus.java
@@ -0,0 +1,5 @@
+public class Bus extends Car {
+    public Bus(String model, String brand, String displacement, int seat, String color, int rent) {
+        super(model, brand, displacement, seat, color, rent);
+    }
+}
