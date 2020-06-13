3cfdbbb61cce0a72e7992cfa75ded690794c490c
zhishouhuang <zhishouhuang@gmail.com>
2020-06-13 21:55:18 +0800

    轿车子类

---
+++ 汽车出租管理程序/Saloon.java
@@ -0,0 +1,5 @@
+public class Saloon extends Car {
+    public Saloon(String model, String brand, String displacement, int seat, String color, int rent) {
+        super(model, brand, displacement, seat, color, rent);
+    }
+}
