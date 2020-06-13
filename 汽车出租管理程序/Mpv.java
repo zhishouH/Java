422b25b2f2e2c73ac268d3e26960719f434d1e03
zhishouhuang <zhishouhuang@gmail.com>
2020-06-13 21:55:08 +0800

    面包车子类

---
+++ 汽车出租管理程序/Mpv.java
@@ -0,0 +1,5 @@
+public class Mpv extends Car {
+    public Mpv(String model, String brand, String displacement, int seat, String color, int rent) {
+        super(model, brand, displacement, seat, color, rent);
+    }
+}
